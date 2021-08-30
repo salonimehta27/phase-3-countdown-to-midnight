f97f15cb8862298c51676bd5abd417c7e0aff43c
Saloni <salonimehta2127@gmail.com>
2021-08-30 16:11:29 -0400

    commit tree d543a435c61f52e85cccf01e93d6aa5fa1bb3ef3

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,18 @@
 #write your code here
+# require 'pry'
+def countdown_with_sleep number
+    sleep number
+   end 
 
-def countdown
+def countdown int 
+    # binding.pry
+    while int>0
+     puts "#{int} SECOND(S)!" 
+     countdown_with_sleep 1
+     int-=1
+    end
+    "HAPPY NEW YEAR!" 
 end
+# countdown 10
+
+
